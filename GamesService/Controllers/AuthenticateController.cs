eb152c8251804cc42c41f22784aa7f0c2ac1b0f7
simasbakus <simas.bakus@gmail.com>
2021-03-01 19:13:36 +0200

    Authenticate controller added

---
+++ GamesService/Controllers/AuthenticateController.cs
@@ -0,0 +1,43 @@
+﻿using GamesService.Models;
+using GamesService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IUserAuthentication _authentication;
+
+        public AuthenticateController(IUserAuthentication authentication)
+        {
+            _authentication = authentication;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult IsAuthenticated()
+        {
+            return Ok("User authenticated");
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            /* AUTHENTICATES USER BY THE CREDENTIALS */
+
+            string token = _authentication.Authenticate(userCred);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
